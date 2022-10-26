3ca56acbd885bb64f85bd3e2df61ba1437cfed85
introbond <introbond.dev@gmail.com>
2022-10-26 21:41:52 +0700

    hash & compare

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 require("dotenv").config();
+var bcrypt = require("bcryptjs");
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    const inputText = "";
+    const mySecret = "";
+    res.render('hash', { inputText, mySecret });
 })
+app.get("/compare", (req, res) => {
+    const inputText = "";
+    const inputHash = "";
+    const output = "";
+    res.render('compare', { inputText, inputHash, output });
+})
+
+app.post("/hashing", async (req, res) => {
+    const { inputText } = req.body;
+    const mySecret = await bcrypt.hash(inputText, 10);
+    res.render('hash', { inputText, mySecret });
+})
+
+app.post("/comparing", async (req, res) => {
+    const { inputText, inputHash } = req.body;
+    let output = "incorrect";
+    if (await bcrypt.compare(inputText, inputHash)) {
+        output = "correct";
+    };
+    res.render('compare', { inputText, inputHash, output });
+})
+
 module.exports = app;
